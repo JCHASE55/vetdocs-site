4e4cf4e229e154a7533db4936a021e61b398818c
JCHASE55 <vetdocs4u@gmail.com>
2025-07-01 11:47:48 -0500

    Update App.js

--- VetDocs_React_Form_Fixed/src/App.js
+++ VetDocs_React_Form_Fixed/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ThankYou from "./pages/ThankYou";
+import ThankYou from "./ThankYou";
 
 function App() {
   return (
