b88832eb9af8e6a7248cd18401742a9ccf246d03
JCHASE55 <vetdocs4u@gmail.com>
2025-07-01 11:46:23 -0500

    Update ThankYou.js

--- VetDocs_React_Form_Fixed/src/ThankYou.js
+++ VetDocs_React_Form_Fixed/src/ThankYou.js
@@ -1,8 +1,10 @@
+import React from "react";
+
 function ThankYou() {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
       <h1>🎉 Thank You!</h1>
-      <p>Your form has been submitted. We’ll be in touch soon.</p>
+      <p>Your form has been submitted. We'll be in touch soon.</p>
       <a href="/">← Back to Home</a>
     </div>
   );
