4cd21ba171f372b319181aa6805d4d3d46bb7f77
JCHASE55 <vetdocs4u@gmail.com>
2025-07-01 11:12:07 -0500

    Create ThankYou.js

---
+++ VetDocs_React_Form_Fixed/src/ThankYou.js
@@ -0,0 +1,11 @@
+function ThankYou() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>🎉 Thank You!</h1>
+      <p>Your form has been submitted. We’ll be in touch soon.</p>
+      <a href="/">← Back to Home</a>
+    </div>
+  );
+}
+
+export default ThankYou;
