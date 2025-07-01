fb6b79447c1d79c3676bf62d065195544432d962
JCHASE55 <vetdocs4u@gmail.com>
2025-07-01 14:40:09 -0500

    Update JunkForm.js

--- VetDocs_React_Form_Fixed/src/JunkForm.js
+++ VetDocs_React_Form_Fixed/src/JunkForm.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function JunkForm() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.google.com/recaptcha/api.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div style={{ padding: "2rem" }}>
       <h2>Junk Removal Request</h2>
       <form
-        action="https://formspree.io/f/mnnvwgnz"
+        action="https://formspree.io/f/YOUR_JUNK_FORM_ID"
         method="POST"
         encType="multipart/form-data"
       >
@@ -26,7 +33,7 @@ function JunkForm() {
         </label>
 
         <label>
-          Describe the Items to Be Removed
+          Description of Junk to Remove
           <textarea name="message" rows="4" />
         </label>
 
@@ -34,6 +41,13 @@ function JunkForm() {
           Upload Photos (Optional)
           <input type="file" name="file" accept=".jpg,.jpeg,.png,.pdf" />
         </label>
+
+        <input type="text" name="_gotcha" style={{ display: "none" }} />
+
+        <div
+          className="g-recaptcha"
+          data-sitekey="6LcwOXQrAAAAADlVAhUetPlQLjRzPILeFecOc0BS"
+        ></div>
 
         <button type="submit">Submit Request</button>
       </form>
